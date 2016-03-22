53212fa269934371517a3a0a98f51578f9ad61e8
Bogdan Mart <mart.bogdan@gmail.com>
2016-03-23 01:48:13 +0200

    Added missing files
    (cherry picked from commit 5a3f9511ca4ddb29a3ef81a652a23a988bf6fb10)

---
+++ WorkWithDB.UI/Debug/BindingTestConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WorkWithDB.UI.Debug
+{
+    public class BindingTestConverter : IValueConverter
+    {
+        private static BindingTestConverter _instance;
+
+        public static BindingTestConverter Instance
+        {
+            get { return _instance ?? (_instance = new BindingTestConverter()); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
