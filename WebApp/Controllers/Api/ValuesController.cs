6f0f4dd8230675bde4a5e3b3fae6214854e9368b
Bogdan Mart <mart.bogdan@gmail.com>
2016-03-22 23:13:25 +0200

    refactoring of API Controller

--- WebApp/Controllers/Api/ValuesController.cs
+++ WebApp/Controllers/Api/ValuesController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
-namespace WebApp.Controllers
+namespace WebApp.Controllers.Api
 {
     public class ValuesController : ApiController
     {
